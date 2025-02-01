5e42dc0ce1bbb576fea836298156ad9636824dfd
khakha <ky.thuat@tlptech.vn>
2025-02-01 14:17:50 +0700

    Huynh Kha mùng 4

--- js/main.js
+++ js/main.js
@@ -21,3 +21,13 @@ const swiper_1 = new Swiper(".banner_mobile", {
   },
   speed: 800,
 });
+
+const swiperProduct = new Swiper(".product_box_mobile", {
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  loop: true,
+  navigation: false,
+  pagination: false,
+  grabCursor: true,
+  centeredSlides: false,
+});
